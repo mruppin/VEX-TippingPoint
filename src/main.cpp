29b0165b832cc7accb008b8a0b73125ea8a3b3b0
camatthew <thecoolkids322@gmail.com>
2021-09-20 15:55:32 -0500

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include <list>
 #include "main.h"
 #include "motors.h"
-
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Motor left_front_wheel (LEFT_FRONT_WHEELS_PORT, true); // This reverses the motor
 pros::Motor left_back_wheel (LEFT_BACK_WHEELS_PORT);
@@ -33,7 +33,12 @@ void disabled() {}
  * starts.
  */
 void competition_initialize() {}
-
+void moveWheels(double left_voltage, double right_voltage){
+		left_front_wheel.move(left_voltage);
+		left_back_wheel.move(left_voltage);
+		right_front_wheel.move(right_voltage);
+		right_back_wheel.move(right_voltage);
+}
 /**
  * Runs the user autonomous code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -69,6 +74,7 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+int speedControl;
 void opcontrol() {
 
   pros::c::adi_pin_mode(2, INPUT);
@@ -83,6 +89,6 @@ void opcontrol() {
 		right_front_wheel.move(master.get_analog(ANALOG_RIGHT_Y));
 		right_back_wheel.move(master.get_analog(ANALOG_RIGHT_Y));
 		    pros::delay(2);
-
+		}
 	}
 }
